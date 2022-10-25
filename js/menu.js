e8e310be739be857076bc7f6db4f95d2668cf0de
Albert1201 <mrafhael12@gmail.com>
2022-10-25 12:30:09 -0600

    Proyecto restaurante

---
+++ js/menu.js
@@ -0,0 +1,6 @@
+let btn_menu = document.getElementById("btn_menu");
+let menu = document.getElementById("menu");
+btn_menu.addEventListener("click", function () {
+    "use strict";
+    menu.classList.toggle("mostrar");
+});
